95c3daa2a9e4f016d329cda7e7a52af93841894e
Desmond Foo <desmondfoo0610@gmail.com>
2023-09-21 17:19:27 +0800

    captivating light

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,98 @@
+import React, { useRef } from 'react';
+import Circle from './Components/Circle';
+import { useEffect } from 'react';
 
+const App = () => {
+    const ref = useRef(null);
+    const width = ref.current ? ref.current.offsetWidth : 0;
 
-function App() {
-
-  return (
-    <>
-      <p className="text-3xl text-green-700">Vite + daReact</p>
-      <h1>Hello my name is desmondfdsfrfdsfs</h1>
-    </>
-  )
+    return (
+      <div className="bg-zinc-800 w-full h-[884px] justify-center items-center inline-flex">
+        <div ref={ref} className="[font-family:'Josefin_Sans-Bold',Helvetica] font-bold text-[#3e3e3e] text-[96px] tracking-[0] leading-[normal] whitespace-nowrap">
+            DESMOND FOO
+        </div>
+        <div className="flex flex-wrap w-[780px] items-center gap-[0px_0px] absolute">
+            <Circle />
+            <Circle />
+            <Circle />
+            <Circle />
+            <Circle />
+            <Circle />
+            <Circle />
+            <Circle />
+            <Circle />
+            <Circle />
+            <Circle />
+            <Circle />
+            <Circle />
+            <Circle />
+            <Circle />
+            <Circle />
+            <Circle />
+            <Circle />
+            <Circle />
+            <Circle />
+            <Circle />
+            <Circle />
+            <Circle />
+            <Circle />
+            <Circle />
+            <Circle />
+            <Circle />
+            <Circle />
+            <Circle />
+            <Circle />
+            <Circle />
+            <Circle />
+            <Circle />
+            <Circle />
+            <Circle />
+            <Circle />
+            <Circle />
+            <Circle />
+            <Circle />
+            <Circle />
+            <Circle />
+            <Circle />
+            <Circle />
+            <Circle />
+            <Circle />
+            <Circle />
+            <Circle />
+            <Circle />
+            <Circle />
+            <Circle />
+            <Circle />
+            <Circle />
+            <Circle />
+            <Circle />
+            <Circle />
+            <Circle />
+            <Circle />
+            <Circle />
+            <Circle />
+            <Circle />
+            <Circle />
+            <Circle />
+            <Circle />
+            <Circle />
+            <Circle />
+            <Circle />
+            <Circle />
+            <Circle />
+            <Circle />
+            <Circle />
+            <Circle />
+            <Circle />
+            <Circle />
+            <Circle />
+            <Circle />
+            <Circle />
+            <Circle />
+            <Circle />
+        </div>
+      </div>
+    )
 }
 
 export default App;
---
+++ src/Components/Circle.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Circle = () => {
+
+    return (
+        <div className="w-[30px] h-[30px] ">
+            <div className="w-[50px] h-[50px] blur-xl hover:bg-amber-400"/>
+        </div>
+
+    );
+};
+
+export default Circle;
